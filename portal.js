39c6060dc115a649c1db3ec21d42809f283cc277
Saulo Henrique <msaulohenrique@users.noreply.github.com>
2017-01-24 20:14:06 -0300

    Update portal.js

--- portal.js
+++ portal.js
@@ -1,112 +1,67 @@
-document.write('\n
-
-<body bgcolor="#807d75">
-
-<br>
-<table width="600" style="margin:0;padding:0;text-align:justify;font-weight:bold;border:none;">
-       <tr">
-           <th><img src="images/gerador/informacoes.png"></th>
-       </tr>
-</table>
-<table border="0" style="table-layout: fixed; width: 600;">
-       <tr>
-           <td style="width: 5px; height: 430px;" rowspan="7"></td>
-           <td style="width: 301px; height: 430px;" rowspan="7"><img src="Capa.jpg"></td>
-           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White">Aquele sentimento de verão</font></td>
-           <td style="width: 5px; height: 430px;" rowspan="7"></td>
-       </tr>
-       <tr>
-           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White">Ano: 2015</font></td>
-       </tr>
-       <tr>
-           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White">MKV - BR-Rip - 1280 x 720</font></td>
-       </tr>
-       <tr>
-           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White">Tamanho: 1,97 Gb</font></td>
-       </tr>
-       <tr>
-           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White">Duracao: 102 min</font></td>
-       </tr>
-       <tr>
-           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White">Idioma: Inglês/Portugues/Espanhol</font></td>
-       </tr>
-       <tr>
-           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White">Legenda: Português/Ingles/Espanhol</font></font></td>
-       </tr>
-</table>
-<br>
-<table width="600" style="margin:0;padding:0;text-align:justify;font-weight:bold;border:none;">
-       <tr style="margin:0;padding:0;border:none;">
-           <th style="margin:0;padding:0;border:none;"><img src="images/gerador/sinopse.png"></th>
-       </tr>
-</table>
-<table border="0" style="table-layout: fixed; width: 600;">
-       <tr>
-           <td style="width: 5px;"></td>
-           <td background="images/gerador/quadro_fundo.png" width="100%" style="margin:0;padding-left: 10px; padding-top: 10px;padding-bottom: 10px;padding-right: 10px;border-left: 3px solid #041727;border-right: 3px solid #75a1c5;border-bottom: 3px solid #041727;border-top: 3px solid #427fb0;">
-           		 <font face="Courier New" color="White">Sasha, uma jovem francesa de 30 anos, morre subitamente na cidade de Berlim, onde vivia com o namorado, Lawrence. Nesse momento de perda, a família e o namorado estão devastados. O trágico evento acaba unindo dois estranhos: o namorado Lawrence e a irmã, Zoé.</font>
-           </td>
-           <td style="width: 5px;"></td>
-       </tr>
-</table>
-<br>
-<table width="600" style="margin:0;padding:0;text-align:justify;font-weight:bold;border:none;">
-       <tr style="margin:0;padding:0;border:none;">
-           <th colspan="3" style="margin:0;padding:0;border:none;"><img src="images/gerador/elenco.png"></th>
-       </tr>
-       <tr style="margin:0;padding:0;border:none;">
-           <td width="1%" style="margin:0;padding:0;border:none;"></td>
-           <td background="images/gerador/quadro_fundo.png" width="100%" style="margin:0;padding:0;border-left: 3px solid #041727;border-right: 3px solid #75a1c5;border-bottom: 3px solid #041727;border-top: 3px solid #427fb0;">
-               <ul style="list-style-type: disc;padding-left: 20px;margin-top: 1em;">
-                   <font face="Courier New" color="White">
-                         <li>Stéphanie Daub-Laurent</li>
-                         <li>Anders Danielsen Lie</li>
-                         <li>Judith Chemla</li>
-                   </font>
-               </ul>
-               </td>
-           <td width="1%" style="margin:0;padding:0;border:none;"></td>
-       </tr>
-</table>
-<br>
-<table width="600" style="margin:0;padding:0;text-align:justify;font-weight:bold;border:none;">
-       <tr style="margin:0;padding:0;border:none;">
-           <th style="margin:0;padding:0;border:none;"><img src="images/gerador/screens.png"></th>
-       </tr>
-</table>
-<img src="Cena01.jpg" width="150px"><img src="Cena02.jpg" width="150px"><img src="Cena03.jpg" width="150px"><img src="Cena04.jpg" width="150px">
-<br>
-<table width="600" style="margin:0;padding:0;text-align:justify;font-weight:bold;border:none;">
-       <tr style="margin:0;padding:0;border:none;">
-           <th style="margin:0;padding:0;border:none;"><img src="images/gerador/trailer.png"></th>
-       </tr>
-</table>
-<table width="600" cellspacing="0" cellpadding="0">
-       <tr>
-           <td colspan="3"><img src="images/gerador/tv-top.png" width="600" height="32"></td>
-       </tr>
-       <tr>
-           <td width="28"><img src="images/gerador/tv-left.png" width="28" height="304"></td>
-           <td width="542">
-               <object width="540" height="300">
-                       <param name="movie" value="http://www.dailymotion.com/swf/x58iuob&amp;v3=1&amp;related=1">
-                       <param name="allowFullScreen" value="true"><param name="allowScriptAccess" value="always">
-                       <embed src="http://www.dailymotion.com/swf/x58iuob&amp;v3=1&amp;related=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="540" height="300">
-               </object>
-           </td>
-           <td width="30"><img src="images/gerador/tv-right.png" width="30" height="304"></td>
-       </tr>
-       <tr>
-           <td colspan="3"><img src="images/gerador/tv-bottom.png" width="600" height="107"></td>
-       </tr>
-</table>
-<br>
-<table width="600" style="margin:0;padding:0;text-align:justify;font-weight:bold;border:none;">
-       <tr style="margin:0;padding:0;border:none;">
-           <th style="margin:0;padding:0;border:none;"><img src="images/gerador/download.png"></th>
-       </tr>
-</table>
-<br>
-<br>
-</body>
+document.write('\n                                                                                                                                                                                                                                                                                                                                                  
+$row = array (
+"titulo"      => "Aquele sentimento de verão",
+"ano"         => 2015,
+"extensao"    => "MKV",
+"qualidade"   => "BR-Rip",
+"resolucao"   => "1280 x 720",
+"tamanho_num" => "1.97",
+"tamanho_car" => "Gb",
+"duracao"     => "102",
+"idioma"      => "Inglês/Portugues/Espanhol",
+"legenda"     => "Português/Inglês/Espanhol",
+"capa"        => "http://www.osninjas.info/ImgTopicos/LordNinja/AqueleSentimentoVerao/Capa.jpg",
+"sinopse"     => "Sasha, uma jovem francesa de 30 anos, morre subitamente na cidade de Berlim, onde vivia com o namorado, Lawrence. Nesse momento de perda, a família e o namorado estão devastados. O trágico evento acaba unindo dois estranhos: o namorado Lawrence e a irmã, Zoé."
+);                                                                                                                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                                                                                                                  
+    $my_result = '';
+    
+    $my_result .= '<table width="600" style="margin:0;padding:0;border:none;">';
+    $my_result .= '       <tr>';
+    $my_result .= '           <th"><img src="images/gerador/informacoes.png"></th>';
+    $my_result .= '       </tr>';
+    $my_result .= '</table>';
+    $my_result .= '<table border="0" style="text-align:center;font-weight:bold;table-layout:fixed;width:600;">';
+    $my_result .= '       <tr>';
+    $my_result .= '           <td style="width: 5px; height: 430px;" rowspan="7"></td>';
+    $my_result .= '           <td style="width: 301px; height: 430px;" rowspan="7"><img src="' . $row['capa'] . '"></td>';
+    $my_result .= '           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White"><font size="1" color="gold">Título<br></font>' . $row['titulo'] . '</font></td>';
+    $my_result .= '           <td style="width: 5px; height: 430px;" rowspan="7"></td>';
+    $my_result .= '       </tr>';
+    $my_result .= '       <tr>';
+    $my_result .= '           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White"><font size="1" color="gold">Ano<br></font>' . $row['ano'] . '</font></td>';
+    $my_result .= '       </tr>';
+    $my_result .= '       <tr>';
+    $my_result .= '           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White"><font size="1" color="gold">Qualidade<br></font>' . $row['extensao'] . ' - ' . $row['qualidade'] . ' - ' . $row['resolucao'] . '</font></td>';
+    $my_result .= '       </tr>';
+    $my_result .= '       <tr>';
+    $my_result .= '           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White"><font size="1" color="gold">Tamanho<br></font>' . $row['tamanho_num'] . ' ' . $row['tamanho_car'] . '</font></td>';
+    $my_result .= '       </tr>';
+    $my_result .= '       <tr>';
+    $my_result .= '           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White"><font size="1" color="gold">Duracao<br></font>' . $row['duracao'] . ' min</font></td>';
+    $my_result .= '       </tr>';
+    $my_result .= '       <tr>';
+    $my_result .= '           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White"><font size="1" color="gold">Idioma<br></font>' . $row['idioma'] . '</font></td>';
+    $my_result .= '       </tr>';
+    $my_result .= '       <tr>';
+    $my_result .= '           <td style="background-image:url(images/gerador/quadro_fundo.png);border-left:2px solid #041727;border-right:2px solid #75a1c5;border-bottom:2px solid #041727;border-top:2px solid #427fb0;"><font face="Courier New" color="White"><font size="1" color="gold"ç>Legenda<br></font>' . $row['legenda'] . '</font></font></td>';
+    $my_result .= '       </tr>';
+    $my_result .= '</table>';
+    $my_result .= '<br>';
+    $my_result .= '<table width="600" style="margin:0;padding:0;border:none;">';
+    $my_result .= '       <tr>';
+    $my_result .= '           <th><img src="images/gerador/sinopse.png"></th>';
+    $my_result .= '       </tr>';
+    $my_result .= '</table>';
+    $my_result .= '<table table border="0" style="text-align:center;font-weight:bold;table-layout:fixed;width:600;">';
+    $my_result .= '       <tr>';
+    $my_result .= '           <td style="width: 5px;"></td>';
+    $my_result .= '           <td background="images/gerador/quadro_fundo.png" width="100%" style="margin:0;padding-left: 10px; padding-top: 10px;padding-bottom: 10px;padding-right: 10px;border-left: 3px solid #041727;border-right: 3px solid #75a1c5;border-bottom: 3px solid #041727;border-top: 3px solid #427fb0;">';
+    $my_result .= '           		 <font face="Courier New" color="White">' . $row['sinopse'] . '</font>';
+    $my_result .= '           </td>';
+    $my_result .= '           <td style="width: 5px;"></td>';
+    $my_result .= '       </tr>';
+    $my_result .= '</table>';
+                                                                                                                                                                                                                                                                                                                                                  
+return $my_result;
 ');
