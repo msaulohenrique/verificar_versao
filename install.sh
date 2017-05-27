2c7a1e78eff0c5932957535be536e5e5a1bda69c
Saulo Henrique <msaulohenrique@users.noreply.github.com>
2017-05-27 00:11:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 echo -e "\033[1;33m ATENÃ‡ÃƒO \033[0m"
 echo "  Este Install foi programado para Instalar o LAMP para distribuiÃ§Ã£o do Linux Debian 8 32x ou 64x."
 echo " Durante a InstalaÃ§Ã£o serÃ¡ solicitado varias vezes para digitar uma senha. Escolha agora uma senha pessoal de sua preferÃªncia"
-echo -e "  Sempre que for solicitado para digite uma \033[1;31mSenha\033[0m ou \033[1;31mPassword\033[0m use sempre esta mesma senha que vocÃª escolheu."
-echo -e "  Utilize uma senha sem caracteres especiais ou espaÃ§o, somente \033[1;31mLETRAS E NÃšMEROS\033[0m."
-echo -e "  \033[1;31mLEMBRE-SE\033[0m desta senha ela serÃ¡ solicitadas varias vezes durante a InstalaÃ§Ã£o."
-echo -e "  Sempre que solicitado \033[1;31m[Y/N]\033[0m escolha a opÃ§Ã£o \033[1;31mY\033[0m."
+echo "  Sempre que for solicitado para digite uma \033[1;31mSenha\033[0m ou \033[1;31mPassword\033[0m use sempre esta mesma senha que vocÃª escolheu."
+echo "  Utilize uma senha sem caracteres especiais ou espaÃ§o, somente \033[1;31mLETRAS E NÃšMEROS\033[0m."
+echo "  \033[1;31mLEMBRE-SE\033[0m desta senha ela serÃ¡ solicitadas varias vezes durante a InstalaÃ§Ã£o."
+echo "  Sempre que solicitado \033[1;31m[Y/N]\033[0m escolha a opÃ§Ã£o \033[1;31mY\033[0m."
 tput setaf 3 ; tput bold ; read -n 1 -s -p "Aperte qualquer tecla para continuar..." ; echo "" ; echo "" ; tput sgr0
 echo "INSTALAÃ‡ÃƒO DO PAINEL SSH 3 INICIADA!"
 apt-get update && apt-get upgrade -y
@@ -92,5 +92,4 @@ echo -e "\033[1;34m          http://seuip/\033[0m"
 echo -e "\033[1;34m          UsuÃ¡rio e Senha Determinados no painel do administrador\033[0m"
 echo ""
 echo "  O reboot e obrigatÃ³rio e serÃ¡ executado automaticamente, apÃ³s ser executado vocÃª pode fechar o terminal e acessar o painel pelo navegador."
-reboot
 exit 1
